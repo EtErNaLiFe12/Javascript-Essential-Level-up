5bf96dc7ebccdb39205343c2bda0eace50f8c242
EtErNaLiFe12 <wnsqja8888@gmail.com>
2021-04-23 17:42:45 +0900

    export 방식 - default / named

--- getRandom.js
+++ getRandom.js
@@ -1,3 +1,11 @@
-export default function random() {
+// export default function () { // default 통로 (기본 통로) 로 나가는 것이기 때문에 뒤에 이름이 필요 없다.
+export function gotit() {
   return Math.floor(Math.random() * 10)
 }
+
+export const user = {
+  name: 'heropy',
+  age: 29
+}
+
+export default 123
--- getType.js
+++ getType.js
@@ -1,3 +1,5 @@
 export default function getType(data) {
   return Object.prototype.toString.call(data).slice(8, -1)
 }
+
+// export default 123 (Only one default export allowed per module.)
